9c1195c775cdf9d17dbaebdc6b6831ba68ebebb1
Mario Servini <31860@bridgend.ac.uk>
2021-11-13 12:54:17 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,16 @@
+def story():
+    print('I will tell you a story, but I need some information first.')
+    name = input('What is the main character called?\n')
+    job = input('What is their job?\n')
+    print('Once upon a time there was,', name, 'who was a', job, '.')
+    print('On the way to work,', name, 'reflected on life.')
+    print('Perhaps', name, 'will not be a', job, 'Data scientist forever.')
+
+
 def main():
-    #write your code below this line
+    # write your code below this line
+    story()
+
 
 if __name__ == '__main__':
     main()
