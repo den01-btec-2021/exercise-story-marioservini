ad27b90e1f15b9c2b22fddf69a4c8b7281372e7f
marioservini <31860@bridgend.ac.uk>
2021-11-16 15:44:52 +0000

    complete

--- src/exercise.py
+++ src/exercise.py
@@ -2,7 +2,7 @@ def story():
     print('I will tell you a story, but I need some information first.')
     name = input('What is the main character called?\n')
     job = input('What is their job?\n')
-    print('Once upon a time there was,', name, 'who was a', job, '.')
+    print('Once upon a time there was,', name, 'who was a', job,'.')
     print('On the way to work,', name, 'reflected on life.')
     print('Perhaps', name, 'will not be a', job, 'Data scientist forever.')
 
